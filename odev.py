935d1af5703985de327dea086cf1889187819eb5
yiğitcan <cakmak02273@gmail.com>
2022-11-15 22:24:44 +0300

    2. düzeltme

--- odev.py
+++ odev.py
@@ -11,8 +11,5 @@ for i in range(0,len(a),1):
 
 # a=a.replace(" ", "A")
 print(a)
-print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
 
